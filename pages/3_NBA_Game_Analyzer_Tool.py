c42ca6309af69081cd2f907da396000a67d34f29
Travis Royce <traviscroyce@gmail.com>
2023-03-17 18:13:26 -0700

    up

--- pages/3_NBA_Game_Analyzer_Tool.py
+++ pages/3_NBA_Game_Analyzer_Tool.py
@@ -57,14 +57,12 @@ from plotly.subplots import make_subplots
 
 st.set_page_config(layout='wide')
 
-
-today = datetime.datetime.today().strftime('%Y-%m-%d')
+date_time = datetime.datetime.now()
+# subtract 7 hours to get to PST
+date_time = date_time - datetime.timedelta(hours=7)
+today = date_time.strftime('%Y-%m-%d')
 st.write(f' the date is {today}')
 
-# get time
-now = datetime.datetime.now()
-current_time = now.strftime("%H:%M:%S")
-st.write(f' the time is {current_time}')
 
 # Load
 today_df = pd.read_csv('data/team/aggregates/daily_updates/today_df_'+str(today)+'.csv', low_memory=False)
